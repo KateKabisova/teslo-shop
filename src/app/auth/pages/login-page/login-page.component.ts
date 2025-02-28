b735fe8cc6df8e5447fe2fc495c10aee0e32f46a
KaterinaKabisova <katekabisova@gmail.com>
2025-02-28 12:32:53 +0100

    fix:auth/login

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AlertComponent } from "../../alert/alert/alert.component";
+import { AuthService } from '@auth/services/auth.service';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [RouterLink, ReactiveFormsModule, AlertComponent],
+  templateUrl: './login-page.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class LoginPageComponent {
+
+  fb = inject(FormBuilder);
+
+  hasError = signal(false);
+  isPosting = signal(false);
+  authService= inject(AuthService);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if(this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
+    this.authService.login(this.loginForm.value.email!, this.loginForm.value.password!).subscribe((resp)=>{
+      console.log(resp);
+    });
+    console.log(this.loginForm.value);
+  }
+
+}
